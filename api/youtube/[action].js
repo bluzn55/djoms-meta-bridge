399f191105d423242809243ac88ef36b3e0a68db
bluzn55 <103379698+bluzn55@users.noreply.github.com>
2026-09-22 11:20:06 -0500

    Route Video Inbox actions

--- api/youtube/[action].js
+++ api/youtube/[action].js
@@ -1,13 +1,23 @@
 import {
   youtubeConnectEndpoint,
   youtubeCallbackEndpoint,
-  youtubeStatusEndpoint
+  youtubeStatusEndpoint,
+  youtubeVideoListEndpoint,
+  youtubeVideoInitEndpoint,
+  youtubeVideoRegisterEndpoint,
+  youtubeVideoSaveEndpoint,
+  youtubeVideoDeleteEndpoint
 } from '../../lib/youtube.js';
 
 const routes = {
   connect: youtubeConnectEndpoint,
   callback: youtubeCallbackEndpoint,
-  status: youtubeStatusEndpoint
+  status: youtubeStatusEndpoint,
+  'video-list': youtubeVideoListEndpoint,
+  'video-init': youtubeVideoInitEndpoint,
+  'video-register': youtubeVideoRegisterEndpoint,
+  'video-save': youtubeVideoSaveEndpoint,
+  'video-delete': youtubeVideoDeleteEndpoint
 };
 
 export default async function handler(req, res) {
